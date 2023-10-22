e5b1145aba03d1bab3fcfa7913cb23527ccf5e72
Ryan Northam <ryan.northam@gmail.com>
2023-10-22 18:44:52 +1100

    remove subcommand

--- cmd/root.go
+++ cmd/root.go
@@ -3,14 +3,16 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	"github.com/ryannortham/digio-task/log"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "digio-task",
 	Short: "Parses a log file containing HTTP requests and to reports on its contents",
 	Long: `
 Parses a log file containing HTTP requests and to reports on its contents
@@ -19,30 +21,37 @@ For a given log file we want to know:
   - The number of unique IP addresses
   - The top 3 most visited URLs
   - The top 3 most active IP addresses
-
-
-         xxxxxx                                    $$$$$$   $$$$$$                          $$$$$$                      
-         xxxxxx       :                            $$$$$$   $$$$$$                          $$$$$$                      
-        xxxxxx    :::::                            $$$$$$   $$$$$$                          $$$$$$                      
-      xxxxxxxx  ::::::::                           $$$$$$                                                               
-  xxxxxxxxxxx ::::::::::   ++            $$$$$$$$$ $$$$$$   $$$$$$      $$$$$$$$$$ $$$$$$   $$$$$$       $$$$$$$$$$     
-xxxxxxxxxxx  ::::::::    +++++         $$$$$$$$$$$$$$$$$$   $$$$$$    $$$$$$$$$$$$$$$$$$$   $$$$$$    $$$$$$$$$$$$$$$$  
-xxxxxxxxx    ::::::     +++++++      $$$$$$$$$$$$$$$$$$$$   $$$$$$   $$$$$$$$$$$$$$$$$$$$   $$$$$$   $$$$$$$$$$$$$$$$$$ 
-xxxxxx      ::::::     ++++++++      $$$$$$       $$$$$$$   $$$$$$  $$$$$$$      $$$$$$$$   $$$$$$  $$$$$$$      $$$$$$$
-            ::::::    +++++++       $$$$$$         $$$$$$   $$$$$$  $$$$$$         $$$$$$   $$$$$$  $$$$$$        $$$$$$
-   ;;;      ::::::   +++++++        $$$$$$         $$$$$$   $$$$$$  $$$$$$         $$$$$$   $$$$$$  $$$$$          $$$$$
- ;;;;;;     ::::::   ++++++         $$$$$$         $$$$$$   $$$$$$  $$$$$$        $$$$$$$   $$$$$$  $$$$$$        $$$$$$
- ;;;;;;;;   ::::     ++++++          $$$$$$       $$$$$$$   $$$$$$   $$$$$$$$$ $$$$$$$$$$   $$$$$$  $$$$$$$      $$$$$$$
-  ;;;;;;;;;          ++++++          $$$$$$$$$$$$$$$$$$$$   $$$$$$   $$$$$$$$$$$$$$$$$$$$   $$$$$$   $$$$$$$$$$$$$$$$$$ 
-    ;;;;;;;;;;;;;;   +++++++           $$$$$$$$$$$$$$$$$$   $$$$$$     $$$$$$$$$$$$$$$$$$   $$$$$$    $$$$$$$$$$$$$$$$  
-     ;;;;;;;;;;;;;;   +++++              $$$$$$$$$ $$$$$$   $$$$$$        $$$$$$   $$$$$$   $$$$$$       $$$$$$$$$$$    
-        ;;;;;;;;;;;    +                                                $$$       $$$$$$                                
-                                                                      $$$$$$$$$$$$$$$$$$                                
-                                                                       $$$$$$$$$$$$$$$$                                 
-                                                                         $$$$$$$$$$$$                                   
-
-
 `,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		logReader := &log.FileReader{Filename: getFilePath()}
+		logParser := &log.CombinedLogParser{}
+		logAnalyzer := &log.LogAnalyzer{}
+
+		// read the log file
+		logLines, err := logReader.ReadLines()
+		if err != nil {
+			fmt.Printf("Error reading log file: %v\n", err)
+			return
+		}
+
+		// parse the log file
+		logEntries, err := logParser.ParseLogEntries(logLines)
+		if err != nil {
+			fmt.Printf("Error parsing log file: %v\n", err)
+			return
+		}
+
+		// analyse the log file data
+		logAnalysis, err := logAnalyzer.AnalyseLogEntries(logEntries)
+		if err != nil {
+			fmt.Printf("Error analysing log file: %v\n", err)
+			return
+		}
+
+		fmt.Print(getDigioLogo())
+		fmt.Printf("unique ip addresses: %d\n", logAnalysis.UniqueIPCount)
+	},
 }
 
 func Execute() {
@@ -65,4 +74,36 @@ func initConfig() {
 	}
 
 	fmt.Printf("Using config file: %s\n", viper.ConfigFileUsed())
+}
+
+func getFilePath() string {
+	dir := viper.GetString("log-dir")
+	fileName := viper.GetString("log-file")
+	return filepath.Join(dir, fileName)
+}
+
+func getDigioLogo() string {
+	return `
+    
+         xxxxxx                                    $$$$$$   $$$$$$                          $$$$$$                      
+         xxxxxx       :                            $$$$$$   $$$$$$                          $$$$$$                      
+        xxxxxx    :::::                            $$$$$$   $$$$$$                          $$$$$$                      
+      xxxxxxxx  ::::::::                           $$$$$$                                                               
+  xxxxxxxxxxx ::::::::::   ++            $$$$$$$$$ $$$$$$   $$$$$$      $$$$$$$$$$ $$$$$$   $$$$$$       $$$$$$$$$$     
+xxxxxxxxxxx  ::::::::    +++++         $$$$$$$$$$$$$$$$$$   $$$$$$    $$$$$$$$$$$$$$$$$$$   $$$$$$    $$$$$$$$$$$$$$$$  
+xxxxxxxxx    ::::::     +++++++      $$$$$$$$$$$$$$$$$$$$   $$$$$$   $$$$$$$$$$$$$$$$$$$$   $$$$$$   $$$$$$$$$$$$$$$$$$ 
+xxxxxx      ::::::     ++++++++      $$$$$$       $$$$$$$   $$$$$$  $$$$$$$      $$$$$$$$   $$$$$$  $$$$$$$      $$$$$$$
+            ::::::    +++++++       $$$$$$         $$$$$$   $$$$$$  $$$$$$         $$$$$$   $$$$$$  $$$$$$        $$$$$$
+   ;;;      ::::::   +++++++        $$$$$$         $$$$$$   $$$$$$  $$$$$$         $$$$$$   $$$$$$  $$$$$          $$$$$
+ ;;;;;;     ::::::   ++++++         $$$$$$         $$$$$$   $$$$$$  $$$$$$        $$$$$$$   $$$$$$  $$$$$$        $$$$$$
+ ;;;;;;;;   ::::     ++++++          $$$$$$       $$$$$$$   $$$$$$   $$$$$$$$$ $$$$$$$$$$   $$$$$$  $$$$$$$      $$$$$$$
+  ;;;;;;;;;          ++++++          $$$$$$$$$$$$$$$$$$$$   $$$$$$   $$$$$$$$$$$$$$$$$$$$   $$$$$$   $$$$$$$$$$$$$$$$$$ 
+    ;;;;;;;;;;;;;;   +++++++           $$$$$$$$$$$$$$$$$$   $$$$$$     $$$$$$$$$$$$$$$$$$   $$$$$$    $$$$$$$$$$$$$$$$  
+     ;;;;;;;;;;;;;;   +++++              $$$$$$$$$ $$$$$$   $$$$$$        $$$$$$   $$$$$$   $$$$$$       $$$$$$$$$$$    
+        ;;;;;;;;;;;    +                                                $$$       $$$$$$                                
+                                                                      $$$$$$$$$$$$$$$$$$                                
+                                                                       $$$$$$$$$$$$$$$$                                 
+                                                                         $$$$$$$$$$$$         
+
+`
 }
